77d716597d3a4e674dfdf78ee924b911049c8ae2
michalhagos <mhayele3@gmail.com>
2026-05-13 00:05:26 -0500

    Added a constructor to Hovercraft  class that calls Vehicle super constructor

--- src/main/java/com/pluralsight/Hovercraft.java
+++ src/main/java/com/pluralsight/Hovercraft.java
@@ -2,7 +2,11 @@ package com.pluralsight;
 
 public class Hovercraft extends Vehicle {
 
-
+    // constructor calls the Vehicle parent constructor using super
+// this sets all the shared fields without duplicating code
+    public Hovercraft(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity) {
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
 
 
 

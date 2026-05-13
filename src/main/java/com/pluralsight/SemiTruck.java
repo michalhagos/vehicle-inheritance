80af7ad54113f39c08703f62a9aca79e8e0adc08
michalhagos <mhayele3@gmail.com>
2026-05-13 00:04:54 -0500

    Added a constructor to SemiTruck class that calls Vehicle super constructor

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -4,7 +4,11 @@ package com.pluralsight;
 // SemiTruck extends Vehicle which means it inherits all fields and methods from the Vehicle class automatically
 public class SemiTruck extends Vehicle{
 
-
+    // constructor calls the Vehicle parent constructor using super
+// this sets all the shared fields without duplicating code
+    public SemiTruck(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity) {
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
 
 
 

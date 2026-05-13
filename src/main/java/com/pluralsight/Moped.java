a0fff514e3c576cb594c93447167bbf8d696d3b4
michalhagos <mhayele3@gmail.com>
2026-05-12 23:54:59 -0500

    Added constructor to Moped class that calls Vehicle super constructor

--- src/main/java/com/pluralsight/Moped.java
+++ src/main/java/com/pluralsight/Moped.java
@@ -3,5 +3,11 @@ package com.pluralsight;
 // Moped extends Vehicle which means it inherits all fields and methods from the Vehicle class automatically
 public class Moped extends Vehicle {
 
+    // constructor calls the Vehicle parent constructor using super
+// this sets all the shared fields without duplicating the previous code
+    public Moped(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity) {
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
+
 
 }
